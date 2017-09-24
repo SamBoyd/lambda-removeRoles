cc1c8d670b072f1af2b2676ed64da6386a1dea24
Sam Boyd <samboyd10@gmail.com>
2017-09-24 18:55:48 +0100

    feat(deploy): Deploy now updates prod package

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,5 @@
 #! /bin/bash
 
-
-#PATH_TO_ZIP=/Users/Sam/Workspace/projects/lambda-getRoles/build/lambda-getRoles.zip
-
 rm lambda.zip | zip -r lambda.zip *
 
-
-#//zip -R ${PATH_TO_ZIP} ./*
-#
-#aws lambda update-function-code         \
-#    --function-name getRoles            \
-#    --zip-file fileb://${PATH_TO_ZIP}   \
-#    --region eu-west-1                  \
-##    --s3-bucket app.samuelrboyd.com     \
-##    --s3-key lambda-getRoles.zip        \
-#
-
+aws lambda update-function-code --function-name arn:aws:lambda:eu-west-1:903390370059:function:removeGoal --zip-file fileb://${pwd}lambda.zip
