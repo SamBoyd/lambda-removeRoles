0a9e4ddcace8a8b0e5eb95c4c6a7137be8671dd2
Sam Boyd <samboyd10@gmail.com>
2017-09-25 09:18:21 +0100

    fix(deploy): Provide the correct package to update the lambda function with

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,5 @@
 
 rm lambda.zip | zip -r lambda.zip *
 
-aws lambda update-function-code --function-name arn:aws:lambda:eu-west-1:903390370059:function:removeRole --zip-file fileb://${pwd}lambda.zip
+aws lambda update-function-code --function-name arn:aws:lambda:eu-west-1:903390370059:function:removeRole --zip-file fileb://$(pwd)/lambda.zip
+
